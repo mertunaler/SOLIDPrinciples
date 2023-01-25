04dcd91086b5457853f531565e1769f9d43feb0e
mertunaler <mertunaler@gmail.com>
2023-01-25 13:15:52 +0300

    Initial Commit

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Rectangle
+{
+    public int Width { get; set; }  
+    public int Height { get; set; }
+
+    public Rectangle()
+    {
+
+    }
+    public Rectangle(int width, int height)
+    {
+        Width= width;
+        Height= height;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Width)} : {Width}, {nameof(Height)}: {Height}";
+    }
+
+
+}
+
+
+
+public class Demo
+{
+    static public int Area(Rectangle r) { return r.Height* r.Width; }
+    public static void Main(string[] args)
+    {
+        Rectangle r = new Rectangle(1,3);
+        Console.WriteLine($"{r} has area {Area(r)}");
+
+    }
+}
